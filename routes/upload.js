310b50e8ae47dcd64baa38ae62e82b00fcae810b
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-04-05 22:28:32 +0200

    changes about firebase

--- routes/upload.js
+++ routes/upload.js
@@ -5,12 +5,17 @@ const fs = require("fs");
 const path = require("path");
 const User = require("../models/users");
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 const { capitalizeFirstLetter } = require("../utils/format");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "seesound-d884f.firebasestorage.app"
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
 });
 
 const bucket = admin.storage().bucket();
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,11 +1,18 @@
-// backend/config/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "seesound-d884f.firebasestorage.app",
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
 });
-console.log("📦 Bucket utilisé :", bucket.name);
 
 const bucket = admin.storage().bucket();
+console.log("📦 Bucket utilisé :", bucket.name);
+
+module.exports = bucket;
