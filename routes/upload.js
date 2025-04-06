293d8fae15d6be5586d42e4d127915e495dcbc68
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-04-06 15:37:31 +0200

    change bucket to firebase

--- routes/upload.js
+++ routes/upload.js
@@ -12,19 +12,7 @@ const serviceAccount = {
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 const { capitalizeFirstLetter } = require("../utils/format");
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // ✅ correct ici !
-  });
-}
-
-const bucket = admin.storage().bucket();
+const bucket = require("../utils/firebase");
 console.log("📦 Bucket utilisé :", bucket.name);
 
 router.post(
