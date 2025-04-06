fb51001ef42b4eb4c7704c332cb742c8bde0699c
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-04-06 15:16:34 +0200

    changes firebase keys

--- routes/upload.js
+++ routes/upload.js
@@ -15,8 +15,12 @@ const { capitalizeFirstLetter } = require("../utils/format");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // ✅ correct ici !
   });
 }
 
