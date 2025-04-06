35f5c2d04f4f2b430652136b1decf0d325f79099
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-04-06 09:49:06 +0200

    modify app.js

--- app.js
+++ app.js
@@ -12,11 +12,24 @@ const uploadRoutes = require("./routes/upload");
 
 const app = express();
 
-app.use(cors({
-    origin: ["http://localhost:3001", "https://seesound-frontend.vercel.app"],
-    methods: ["POST", "GET"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+const allowedOrigins = [
+  "http://localhost:3001",
+  "https://seesound-frontend.vercel.app",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // préflight request
+  }
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
